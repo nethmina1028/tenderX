b2e6287a753b545bed3552b42c84d312c9f03112
nethmina1028 <nethmina1028@gmail.com>
2025-09-04 22:16:41 +0530

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <>
+      <div className="">
+        <Navbar />
+        <h1>scss</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+
+
+function Navbar() {
+  return (
+     <nav className="w-full bg-primary p-6 shadow-md ">
+       <div className="flex items-center justify-center gap-8">
+          <h2 className="text-lg text-white">Home</h2>
+            <a href="/" className="text-white text-3xl font-bold">
+                TenderX
+            </a>  
+            <h2 className="text-lg text-white">Tenders</h2>
+       </div>
+        </nav>
+  )
+}
+
+export default Navbar
